d87337ea86f527d421dee942d22bafe1b1dac201
李小明 <likeming@jasgroup.cn>
2019-10-14 11:36:22 +0800

    kafka修改参数名称

--- kafka/resource/entrypoint.sh
+++ kafka/resource/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 KAFKA_CONFIG="/server/kafka/config/server.properties"
 
 # 替换IP和端口
-sed -i "s/advertised.listeners=PLAINTEXT\:\/\/.*/advertised.listeners=PLAINTEXT\:\/\/${IP}\:${PORT}/g" "${KAFKA_CONFIG}"
+sed -i "s/advertised.listeners=PLAINTEXT\:\/\/.*/advertised.listeners=PLAINTEXT\:\/\/${KAFKA_HOST}/g" "${KAFKA_CONFIG}"
 
 # 服务异常关闭后,如果不清理此目录会因为broker.id重复而造成kafka无法启动.
 rm -rf /server/data/zookeeper
