dfcd6d8eec1cb3816da45263746b1b86aa99034e
李小明 <likeming@jasgroup.cn>
2020-01-07 11:02:01 +0800

    修正openj9-kafka异常重启后无法启动问题

--- openj9-kafka/resource/entrypoint.sh
+++ openj9-kafka/resource/entrypoint.sh
@@ -7,8 +7,10 @@ KAFKA_CONFIG="/server/kafka/config/server.properties"
 # 替换IP和端口
 sed -i "s/advertised.listeners=PLAINTEXT\:\/\/.*/advertised.listeners=PLAINTEXT\:\/\/${KAFKA_HOST}/g" "${KAFKA_CONFIG}"
 
-# 服务异常关闭后,如果不清理此目录会因为broker.id重复而造成kafka无法启动.
+# 服务异常关闭后,如果不清理此目录,会因为broker.id重复而造成kafka无法启动.
 rm -rf /server/data/zookeeper
+# 服务异常关闭后,如果不清理此目录,会因为meta.properties中的clusterId不匹配而造成kafka无法启动.
+rm -rf /tmp/kafka-logs
 
 /server/zookeeper/bin/zkServer.sh start-foreground &
 /server/kafka/bin/kafka-server-start.sh -daemon "${KAFKA_CONFIG}"
