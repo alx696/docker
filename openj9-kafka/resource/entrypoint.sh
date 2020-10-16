c740d27f71a77aa48178f3cb62928c7b786c087c
李小明 <likeming@jasgroup.cn>
2020-10-16 11:28:07 +0800

    修正端口配置不同时报错无法启动问题.

--- openj9-kafka/resource/entrypoint.sh
+++ openj9-kafka/resource/entrypoint.sh
@@ -6,14 +6,19 @@ if [ -z "$IP" ]; then
   echo "没有设置IP!"
   exit 1
 fi
-# 如未设置给默认值
-if [ -z "$PLAINTEXT_PORT" ]; then
-  PLAINTEXT_PORT="9092"
+
+# 根据端口配置设置监听
+LISTENERS="PLAINTEXT:\/\/0.0.0.0:9092"
+ADVERTISED_LISTENERS="PLAINTEXT:\/\/localhost:9092"
+if [ ! -z "$PLAINTEXT_PORT" ]; then
+  ADVERTISED_LISTENERS="PLAINTEXT:\/\/${IP}:${PLAINTEXT_PORT}"
 fi
-# 如未设置给默认值
-if [ -z "$SSL_PORT" ]; then
-  SSL_PORT="9093"
+
+if [ ! -z "$SSL_PORT" ]; then
+  LISTENERS="${LISTENERS},SSL:\/\/0.0.0.0:9093"
+  ADVERTISED_LISTENERS="${ADVERTISED_LISTENERS},SSL:\/\/${IP}:${SSL_PORT}"
 fi
+
 # 如未设置给默认值
 if [ -z "$KEYSTORE_PASSWORD" ]; then
   KEYSTORE_PASSWORD="123456"
@@ -21,7 +26,8 @@ fi
 
 # 替换
 KAFKA_CONFIG="/server/kafka/config/server.properties"
-sed -i "s/advertised.listeners=.*/advertised.listeners=PLAINTEXT:\/\/${IP}:${PLAINTEXT_PORT},SSL:\/\/${IP}:${SSL_PORT}/g" ${KAFKA_CONFIG}
+sed -i "s/listeners=.*/listeners=${LISTENERS}/g" ${KAFKA_CONFIG}
+sed -i "s/advertised.listeners=.*/advertised.listeners=${ADVERTISED_LISTENERS}/g" ${KAFKA_CONFIG}
 sed -i "s/ssl.keystore.password=.*/ssl.keystore.password=${KEYSTORE_PASSWORD}/g" ${KAFKA_CONFIG}
 
 # 服务异常关闭后,如果不清理此目录,会因为broker.id重复而造成kafka无法启动.
@@ -34,5 +40,3 @@ rm -rf /tmp/kafka-logs
 
 #Don`t exit!
 tail -f /dev/null
-
-exec "$@"
